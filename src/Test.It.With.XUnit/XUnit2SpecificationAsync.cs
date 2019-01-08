dd8bb3740cdf555e30557b5fd53bdf0e7b288c5e
Fredrik Arvidsson <fredrik.arvidsson@betssongroup.com>
2019-01-08 14:35:19 +0100

    add async xunit 2 specification

---
+++ src/Test.It.With.XUnit/XUnit2SpecificationAsync.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Test.It.Specifications;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Test.It.With.XUnit
+{
+    public abstract class XUnit2SpecificationAsync : SpecificationAsync, IAsyncLifetime
+    {
+        private readonly DisposableList _disposableList = new DisposableList();
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        protected XUnit2SpecificationAsync()
+            : this(new NoTestOutputHelper())
+        {
+        }
+
+        protected XUnit2SpecificationAsync(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+
+            if (_testOutputHelper.GetType() != typeof(NoTestOutputHelper))
+            {
+                SetupOutput();
+            }
+        }
+
+        private void SetupOutput()
+        {
+            _disposableList.Add(Output.WriteTo(_testOutputHelper));
+        }
+
+        protected readonly TextWriter TestOutputHelper = Output.Writer;
+
+        protected virtual Task DisposeAsync(bool disposing)
+        {
+            _disposableList.Dispose();
+            return Task.CompletedTask;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await SetupAsync();
+        }
+
+        public async Task DisposeAsync()
+        {
+            await DisposeAsync(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ tests/Test.It.With.XUnit.Tests/XUnit2SpecificationAsyncTester.cs
@@ -0,0 +1,14 @@
+﻿using Xunit.Abstractions;
+
+namespace Test.It.With.XUnit.Tests
+{
+    public class XUnit2SpecificationTesterAsync : XUnit2SpecificationAsync
+    {
+        public XUnit2SpecificationTesterAsync(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            OutputHelper = testOutputHelper;
+        }
+
+        protected ITestOutputHelper OutputHelper { get; }
+    }
+}
